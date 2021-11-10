1f41607337ffc3e3d0fe45d830eb0957c95d704d
vishnu <vishnu@mobiboxsoftech.com>
2021-11-10 16:00:17 +0530

    1.9.8

--- MobFlowiOS/NotificationLayout1.swift
+++ MobFlowiOS/NotificationLayout1.swift
@@ -7,15 +7,18 @@
 
 import UIKit
 
-class NotificationLayout1: UIViewController {
+public class NotificationLayout1: UIViewController {
 
     @IBOutlet weak private var layoutTitle : UILabel!
     @IBOutlet weak private var layoutDesciption : UILabel!
     @IBOutlet weak private var layoutBackgroundImage: UIImageView!
     @IBOutlet weak private var layoutCloseButton : UIButton!
-    @IBOutlet weak private var loadMoreButton : UIButton
-        
-    override func viewDidLoad() {
+    @IBOutlet weak private var loadMoreButton : UIButton!
+    
+    
+    
+    
+    public override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         layoutTitle.text = "Do any additional setup after loading the view"
